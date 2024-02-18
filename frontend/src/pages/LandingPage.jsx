5d375a6e2b36f504b0236479cac2f4900475a3d8
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-18 17:16:50 +0100

    feat: created landing page (body) component

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Box from '@mui/system/Box';
+
+const LandingPage = () => {
+  return (
+    <Box component="section" sx={{ p: 2, border: '1px dashed grey', height:"60vh" }}>
+      This is a section container
+    </Box>
+  );
+}
+
+export default LandingPage;
