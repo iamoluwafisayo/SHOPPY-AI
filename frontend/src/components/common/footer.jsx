a4d1355c4dd419d7649bf79bc12d1b39896343c5
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-18 17:15:52 +0100

    feat: created footer

---
+++ frontend/src/components/common/footer.jsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Box, List, ListItem, ListItemText } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+
+const Footer = () => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      component="footer"
+      sx={{
+        background: theme.palette.background.paper,
+        p: 6,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={6} justifyContent="space-evenly">
+          <Grid item xs={12} sm={6} md={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Home
+            </Typography>
+            <List dense={true}>
+              <ListItem sx={{ height: 4 }}>
+                <ListItemText primary="About" />
+              </ListItem>
+              <ListItem sx={{ height: 40 }}>
+                <ListItemText primary="Privacy Policy" />
+              </ListItem>
+              <ListItem sx={{ height: 30 }}>
+                <ListItemText primary="Terms of Service" />
+              </ListItem>
+            </List>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Services
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              We are XYZ company, dedicated to providing the best service to our
+              customers.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              About Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              We are XYZ company, dedicated to providing the best service to our
+              customers.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              122 Main Street, Anytown, USA
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: info@example.com
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Phone: +1 224 567 8901
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={6} md={2}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/" color="inherit">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+            <Link href="https://www.twitter.com/" color="inherit">
+              <Twitter />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://shoppyai.com/">
+              Shoppy Ai
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
