d925eedcf05eea0c45cf2a3cfeee6301d6ba7a1f
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-21 20:22:57 +0100

    refactor: moved navbar to Landing page component, renamed landingPage Directory

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,20 +1,28 @@
 import * as React from "react";
 import { Box, Container } from "@mui/material";
-import Section1 from "../components/landingPageSections.jsx/Section1";
-import Section2 from "../components/landingPageSections.jsx/Section2";
-import Section3 from "../components/landingPageSections.jsx/Section3";
-import Section4 from "../components/landingPageSections.jsx/Section4";
+import ResponsiveNavBar from "../components/common/navbar";
+import Section1 from "../components/landingPageSections/Section1";
+import Section2 from "../components/landingPageSections/Section2";
+import Section3 from "../components/landingPageSections/Section3";
+import Section4 from "../components/landingPageSections/Section4";
+import Section5 from "../components/landingPageSections/Section5";
+import Section6 from "../components/landingPageSections/Section6";
+import Footer from "../components/common/footer";
 
 const LandingPage = () => {
   return (
     <Container maxWidth="xl">
+      <ResponsiveNavBar />
       <Box component="section" sx={{ height: "auto", marginTop: "80px" }}>
         <Section1 />
         <Section2 />
         <Section3 />
         <Section4 />
+        <Section5 />
+        <Section6 />
       </Box>
-    </Container>
+      <Footer />
+    </Container>   
   );
 };
 
