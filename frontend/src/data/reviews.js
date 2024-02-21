a4e97fc459a74186856c840d6d98cd6831a59c8b
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-21 20:29:22 +0100

    feat: review data added

---
+++ frontend/src/data/reviews.js
@@ -0,0 +1,38 @@
+export const reviews = [
+  {
+    id: 1,
+    first_name: "John",
+    last_name: "Abraham",
+    role: "Consultant",
+    created_at: "9 Mins Ago",
+    rating: 4,
+    review:
+      "The seamless integration of artificial intelligence has revolutionized our customer service. Quick and accurate responses, personalized interactions, and efficient issue resolution have elevated our customer satisfaction to new heights. The platform's intuitive design and robust capabilities have significantly streamlined our support processes.",
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/2d3b/adc9/1f7dfe94d316c2c196136c45cf110676?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KX3N-QKGHem7UlSTlPPzXT5h2L580ULuWthrlGm0aEoT70mYNB7AgD4lcQrU4TUf4NfM6Ah7mzfHUWBwmZeNNXVf-eED77OnR6scLlKgIZ54Btw4f4ht~NsWiDvAcrOT94YCpghcDZSOJRUIUokmaj5EJ17mzwt2ShB9WopZzR1dI8v532c7DUpea1zcEmZSiwJcPF3kjL6bi7loNS83c7nGJww7Fpeu81~8vhkAyTdqgUqQOX9QSS8AqMey-enC0MsoksiXriq6N0oKN0vCgzNGq38mxY0IEbC8Zh9aU-QlrWLNcl0BDsQzWhRcmNJAQbVu3QdBPll1v~H2T9Lr0g__",
+  },
+  {
+    id: 2,
+    first_name: "David",
+    last_name: "Thompson",
+    role: "Consultant",
+    created_at: "9 Mins Ago",
+    rating: 3.3,
+    review:
+      "The seamless integration of artificial intelligence has revolutionized our customer service. Quick and accurate responses, personalized interactions, and efficient issue resolution have elevated our customer satisfaction to new heights. The platform's intuitive design and robust capabilities have significantly streamlined our support processes.",
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/e3c1/02e2/57ef0ef0c0f319084ce7a1371f0b32c0?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mv6hMrdunveokk2IGE5oy6XlJLQroxXc5N90SdtXjXtOJpKRwPP57bQlivBrEaa~SABsv0YxpWlTQ6EZZcxWqYmfOk1lNL8z8xjD932aq7ftJGQeSWw4R53RfjmkfS8NjdEReBm-HTierLCsro7GJUkzO6K0MEeeKfA2oKit5378vBTb2h1P2okelQrkP9PSG86gPhcSH9izjFBaPfDeEfL8B5yYdYWzZe-Wy3jqSey0hduZHaLl7WrtF4hC5oBCdSyMI39L6j1J~x1-ApZFrMK92LMEcN0-NoEqp2rCOvu3~VTYHjmulzwP050cKkXAymzmHvK3vWvEUwVf26ywWg__",
+  },
+  {
+    id: 3,
+    first_name: "Sam",
+    last_name: "Newton",
+    role: "Consultant",
+    created_at: "9 Mins Ago",
+    rating: 3.8,
+    review:
+      "The seamless integration of artificial intelligence has revolutionized our customer service. Quick and accurate responses, personalized interactions, and efficient issue resolution have elevated our customer satisfaction to new heights. The platform's intuitive design and robust capabilities have significantly streamlined our support processes.",
+    imageUrl:
+      "https://s3-alpha-sig.figma.com/img/5cc7/681a/676fb6a1fc6974e8c7b030873357b0ac?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WONYIdjdl3oWJDzqPS3U8xY8ZTfyJVI62kZcGPCc7w45mWSVobEs-ayAQBMgdZ9mgBESbBBHeI~9AGTOtt6SsGL2-0BGFWCoNQFrhizW33XGdTXFapeYt069EgW0lLvGUnQWEZSNJTdarsg5feVlCLk6WaOCx1BK1AGtRDPRsOzTtBYz1u4xNXjdxFEUpP2TTBUjnDwYPyFod5kVVIvu-Gyau4hsikmdu7-Q1yJ2-7zEpnOjmzlDq5758beqasUpudZmsUviXpxK8RfpCnYG9f3YoZDobVp3tkDz7fEABuS6BkSSErdPg2yFmc4JFEm-nLRdME5-QUqvJC45-y4E9w__",
+  },
+];
