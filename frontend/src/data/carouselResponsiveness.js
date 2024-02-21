da98efedbe62ce1506952d95c7ef9b822e2af36d
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-21 20:28:58 +0100

    feat: review configuration added

---
+++ frontend/src/data/carouselResponsiveness.js
@@ -0,0 +1,17 @@
+export const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+  mobile: {
+    breakpoint: { max: 768, min: 0 },
+    items: 1,
+    slidesToSlide: 1,
+  },
+};
