d6560c6a430faeee60a3ba42521d6e3e8d67f881
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-21 20:18:47 +0100

    refactor: moved navbar to Landing page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import ResponsiveNavBar from "./components/common/navbar";
-import Footer from "./components/common/footer";
 import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <React.Fragment>
-      <ResponsiveNavBar />
-      {/* body */}
       <LandingPage />
-      <Footer/>
     </React.Fragment>
   );
 };
