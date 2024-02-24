3e3be1c049e3f873984ecab5757f5c46b301b80b
okwir francis <okwirfrancis2010@gmail.com>
2024-02-24 06:15:38 +0300

    Created login/register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
     <React.Fragment>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>      
     </React.Fragment>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeToggleProvider } from "./contexts/ThemeContext";
 import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeToggleProvider>
+  <BrowserRouter>
+   <ThemeToggleProvider>
       <CssBaseline>
         <App />
       </CssBaseline>
     </ThemeToggleProvider>
+  </BrowserRouter>   
   </React.StrictMode>
 );
