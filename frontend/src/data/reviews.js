78d46e38d6cc5ec81e9895bdd9a7837a82c04a70
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-26 14:00:57 +0100

    modified:   frontend/src/data/reviews.js

--- frontend/src/data/reviews.js
+++ frontend/src/data/reviews.js
@@ -4,7 +4,7 @@ export const reviews = [
     first_name: "John",
     last_name: "Abraham",
     role: "Consultant",
-    created_at: "9 Mins Ago",
+    created_at: "15 Mins Ago",
     rating: 4,
     review:
       "The seamless integration of artificial intelligence has revolutionized our customer service. Quick and accurate responses, personalized interactions, and efficient issue resolution have elevated our customer satisfaction to new heights. The platform's intuitive design and robust capabilities have significantly streamlined our support processes.",
@@ -13,10 +13,10 @@ export const reviews = [
   },
   {
     id: 2,
-    first_name: "David",
-    last_name: "Thompson",
+    first_name: "Elizabeth",
+    last_name: "Jane",
     role: "Consultant",
-    created_at: "9 Mins Ago",
+    created_at: "17 Mins Ago",
     rating: 3.3,
     review:
       "The seamless integration of artificial intelligence has revolutionized our customer service. Quick and accurate responses, personalized interactions, and efficient issue resolution have elevated our customer satisfaction to new heights. The platform's intuitive design and robust capabilities have significantly streamlined our support processes.",
@@ -27,8 +27,8 @@ export const reviews = [
     id: 3,
     first_name: "Sam",
     last_name: "Newton",
-    role: "Consultant",
-    created_at: "9 Mins Ago",
+    role: "C.E.O Google Inc",
+    created_at: "2 Hours Ago",
     rating: 3.8,
     review:
       "The seamless integration of artificial intelligence has revolutionized our customer service. Quick and accurate responses, personalized interactions, and efficient issue resolution have elevated our customer satisfaction to new heights. The platform's intuitive design and robust capabilities have significantly streamlined our support processes.",
