cf3cac532f55080ce039ec0e7efcb1bd32428b92
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-26 13:57:08 +0100

    feat: page router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeToggleProvider } from "./contexts/ThemeContext";
 import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeToggleProvider>
       <CssBaseline>
-        <App />
+        <Router>
+          <App />
+        </Router>
       </CssBaseline>
     </ThemeToggleProvider>
   </React.StrictMode>
