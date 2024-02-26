3b470fdc1d23a19820638a5db38240170815f51f
iamoluwafisayo <bosededavid2000@gmail.com>
2024-02-26 13:56:10 +0100

    feat: add additional pages (SigninPage, SignupPage) and routed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import LandingPage from "./pages/LandingPage";
+import { LandingPage, SigninPage, SignupPage } from "./pages";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <React.Fragment>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth/signin/" element={<SigninPage />} />
+        <Route path="/auth/signup/" element={<SignupPage />} />
+      </Routes>
     </React.Fragment>
   );
 };
