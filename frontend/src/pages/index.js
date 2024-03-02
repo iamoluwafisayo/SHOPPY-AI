cb2268df7bdbc538a84fd6372139d14cea1f1682
iamoluwafisayo <bosededavid2000@gmail.com>
2024-03-02 03:26:31 +0100

    modified:   frontend/src/pages/index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,7 @@
 export { default as LandingPage } from "./LandingPage";
 export { default as SigninPage } from "./SigninPage";
 export { default as SignupPage } from "./SignupPage";
+export { default as ResetPassword } from "./ResetPassword";
+export { default as ResetPasswordConfirm } from "./ResetPasswordConfirm";
+export { default as OTPAuth } from "./OTPAuths"
+export { default as SuccessPage } from "./SuccessPage";
