5f3f2887f05588ec04e2055a132f11520eb54876
iamoluwafisayo <bosededavid2000@gmail.com>
2024-03-02 03:23:51 +0100

    feat: add OTP reducer

---
+++ frontend/src/components/reducers/OTPReducer.js
@@ -0,0 +1,41 @@
+import ACTIONS from "./actions";
+
+export const OTPReducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.SET_OTP_RESEND_BUTTON:
+      return {
+        ...state,
+        isResendButtonDisabled: action.payload,
+      };
+    case ACTIONS.SET_OTP_TIMER:
+      return {
+        ...state,
+        timer: action.payload,
+      };
+    case ACTIONS.SET_OTP:
+      return {
+        ...state,
+        otp: action.payload.newOTP,
+      };
+    case ACTIONS.SET_OTP_ERROR:
+      return {
+        ...state,
+        otpError: action.payload.otpError,
+      };
+    case ACTIONS.SET_TIMER_AND_BTN:
+      return {
+        ...state,
+        isResendButtonDisabled: action.payload.button,
+        timer: action.payload.timer,
+      };
+
+    case ACTIONS.SET_TIMER:
+      if (state.timer <= 1) {
+        clearInterval(action.payload.interval);
+        return { ...state, timer: 0, isResendButtonDisabled: false };
+      }
+      return { ...state, timer: state.timer - 1 };
+    default:
+      return state;
+  }
+};
